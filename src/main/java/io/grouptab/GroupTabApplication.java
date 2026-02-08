e2abf80f83823bdd8706a608e8340f337ef35bff
rickysurya <rickysuryaa10@gmail.com>
2026-02-09 01:00:24 +0700

    Initial commit

---
+++ src/main/java/io/grouptab/GroupTabApplication.java
@@ -0,0 +1,13 @@
+package io.grouptab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupTabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupTabApplication.class, args);
+	}
+
+}
