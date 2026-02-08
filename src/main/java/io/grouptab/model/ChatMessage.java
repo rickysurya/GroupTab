d9a1c3907936a64b1ce9a82810536ec8acc455c7
rickysurya <rickysuryaa10@gmail.com>
2026-02-09 03:39:44 +0700

    Making the chat functionality work
    Signed-off-by: rickysurya <rickysuryaa10@gmail.com>

---
+++ src/main/java/io/grouptab/model/ChatMessage.java
@@ -0,0 +1,23 @@
+package io.grouptab.model;
+
+public class ChatMessage {
+    private String content;
+    private String username;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
